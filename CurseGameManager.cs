e86e903d78d7927cd8be40d46ac1fe8b193e8a94
Cmkramer62 <Cameronkramer62@gmail.com>
2026-01-17 17:05:49 -0800

    Test push.

--- CurseGameManager.cs
+++ CurseGameManager.cs
@@ -26,6 +26,7 @@ public class CurseGameManager : MonoBehaviour {
     public GhostRandomizer ghostRandomizer;
     //public ParticleSystem auraEnviroParticles;
     
+    //test push
 
     // Start is called before the first frame update
     void Start() {
