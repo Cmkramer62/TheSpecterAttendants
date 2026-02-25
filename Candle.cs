75fd31f34dfe08d28a2689d487c588df76c4afeb
Cmkramer62 <Cameronkramer62@gmail.com>
2026-02-24 22:40:39 -0800

    Candle logic fixed.

--- Candle.cs
+++ Candle.cs
@@ -8,19 +8,33 @@ public class Candle : MonoBehaviour {
     public ParticleSystem candlefireParticle, smokeParticle;
     public bool candleState = false;
 
-    private CandleSystem candleSystem;
+    //private CandleSystem candleSystem;
 
     private void OnEnable() {
-        candleSystem = GameObject.Find("Game Manager").GetComponent<CandleSystem>();
+        //candleSystem = GameObject.Find("Game Manager").GetComponent<CandleSystem>();
+        if(candleState) { // candle is off
+            candleEffects.SetActive(true);
+            candlefireParticle.Play();
+        }
+        else {
+            candleEffects.SetActive(false);
+            candlefireParticle.Stop();
+        }
     }
 
     public void InteractWithCandle() {
-        if(!candleState) {
+        if(!candleState) { // candle is off
             candleEffects.SetActive(true);
             candlefireParticle.Play();
             candleState = true;
-            gameObject.layer = 0;
-            candleSystem.CandleActivate();
+           // gameObject.layer = 0;
+            //candleSystem.CandleActivate();
+        }
+        else {
+            candleEffects.SetActive(false);
+            candlefireParticle.Stop();
+            candleState = false;
+           // gameObject.layer = 0;
         }
     }
 
