12b434065db042741ce35a806007176c6345a650
Cmkramer62 <Cameronkramer62@gmail.com>
2026-03-15 02:34:02 -0700

    PostRound sequence.

--- Homebase.cs
+++ Homebase.cs
@@ -9,6 +9,9 @@ public class Homebase : MonoBehaviour {
     public TextMeshProUGUI resultsText;
     public GameObject firstMessage;
 
+    [SerializeField] private PostRoundManager postRoundScript;
+
+
     // Start is called before the first frame update
 
     private void OnEnable() {
@@ -24,27 +27,29 @@ public class Homebase : MonoBehaviour {
 
     private void HandleSaveLoaded() {
         Debug.Log("Save finished loading! " + saveSystem.level);
-
+        
         if(saveSystem.level == -1) {
-            resultsText.gameObject.GetComponent<TextAdder>().endWord = "Mission: Failed";
-            resultsText.gameObject.SetActive(true);
+            DisplayPostRoundResults(false, 333, 2, 4, 13);
+            //resultsText.gameObject.GetComponent<TextAdder>().endWord = "Mission: Failed";
+            //resultsText.gameObject.SetActive(true);
         }
         else if(saveSystem.level == 1) {
-            resultsText.gameObject.GetComponent<TextAdder>().endWord = "Mission: Success";
-            resultsText.gameObject.SetActive(true);
+            DisplayPostRoundResults(true, 333, 2, 4, 13);
+            //resultsText.gameObject.GetComponent<TextAdder>().endWord = "Mission: Success";
+            //resultsText.gameObject.SetActive(true);
         }
         else {
-            resultsText.gameObject.GetComponent<TextAdder>().endWord = "Welcome";
-            resultsText.gameObject.SetActive(true);
+            // don't call display postround results at all, because we got to here (the hub) from somewhere other than a mission.
+            //resultsText.gameObject.GetComponent<TextAdder>().endWord = "Welcome";
+            //resultsText.gameObject.SetActive(true);
         }
-
-        firstMessage.SetActive(saveSystem.hubFirstData);
-
-        // InitializeMySystem(level);
-    }
-
-    void Start() {
         
+        firstMessage.SetActive(saveSystem.hubFirstData);
     }
+
+    public void DisplayPostRoundResults(bool success, int secondsSpent, int livesLeft, int timeSpotted, int longestChase) {
+        postRoundScript.DisplayPostRoundResults(success, secondsSpent, livesLeft, timeSpotted, longestChase);
+    }
+
 
 }
